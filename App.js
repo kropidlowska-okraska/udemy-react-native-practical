33c018eb0f52a15cf3d9dd5ce36f30dc27d78821
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-13 10:53:09 +0100

    Add Modal, background color and Image

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { StyleSheet, View, Button, Text, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
   const addGoalHandler = (enteredGoal) => {
     setGoals(currentGoals => ([...currentGoals, { text: enteredGoal, id: Math.random().toString() }]));
@@ -18,15 +20,16 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput addGoalHandler={addGoalHandler}/>
+      <Button title="Add new goal" color="#a07fcc" onPress={() => setModalIsVisible(true)}/>
+      <GoalInput addGoalHandler={addGoalHandler} modalIsVisible={modalIsVisible} setModalIsVisible={setModalIsVisible}/>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
         <FlatList
           data={goals}
           renderItem={itemData => <GoalItem text={itemData.item.text} deleteItem={() => deleteGoalHandler(itemData.item.id)} />}
           keyExtractor={item => item.id}
         />
       </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -36,6 +39,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
+    backgroundColor: '#431331',
   },
   goalsContainer: {
     flex: 5,
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import { StyleSheet, TextInput, View, Button, Modal, Image } from "react-native";
 
-const GoalInput = ({ addGoalHandler }) => {
+const GoalInput = ({ addGoalHandler, modalIsVisible, setModalIsVisible }) => {
     const [enteredGoal, setEnteredGoal] = useState('');
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
@@ -9,19 +9,27 @@ const GoalInput = ({ addGoalHandler }) => {
 
     const onAddGoalHandler = () => {
         addGoalHandler(enteredGoal);
+        setModalIsVisible(false);
         setEnteredGoal('');
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                placeholder="Your course goal!"
-                onChangeText={goalInputHandler}
-                value={enteredGoal}
-            />
-            <Button title="Add goal" onPress={onAddGoalHandler} />
-        </View>
+        <Modal visible={modalIsVisible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <Image source={require('../assets/images/goal.png')} style={styles.image} />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Your course goal!"
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <View style={styles.buttonsContainer}>
+                    <Button title="Add goal" onPress={onAddGoalHandler} color="#ab54ce" />
+                    <Button title="Cancel" onPress={() => setModalIsVisible(false)} color="#dba5b2" />
+                </View>
+
+            </View>
+        </Modal>
     )
 }
 
@@ -35,14 +43,26 @@ const styles = StyleSheet.create({
         width: '70%',
         marginRight: 8,
         padding: 8,
+        color: 'white',
     },
     inputContainer: {
         flex: 1,
+        justifyContent: 'center',
+        gap: 16,
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#4a148c',
+    },
+    buttonsContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
+        width: '70%',
     },
+    image: {
+        width: 200,
+        height: 200,
+        alignSelf: "center",
+        marginVertical: 30,
+        borderRadius: 5,
+    }
 })
